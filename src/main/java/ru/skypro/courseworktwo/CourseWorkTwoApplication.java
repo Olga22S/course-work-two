42c5a82f44022f021e29723c8447c00a43e1e0d3
Olga22S <22031992Les>
2022-03-03 22:49:44 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/courseworktwo/CourseWorkTwoApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.courseworktwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/courseworktwo/CourseWorkTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.courseworktwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
